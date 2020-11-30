c03e5e1c843769e0d2619e1e7a41eac37d5b6066
Bahram Abediniangrabi <abedini.bahram@gmail.com>
2020-11-30 10:55:48 -0600

    Cleaning the code
    Cleaning the code

--- R/ConstructionAnalytics.R
+++ R/ConstructionAnalytics.R
@@ -14,8 +14,7 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
   options(warn=-1)
     ### Note:
     # Calculates option prices from the Cox-Ross-Rubinstein Binomial tree model.
-    #   The model described here is a version of the CRR Binomial Tree model.
-
+    # The model described here is a version of the CRR Binomial Tree model.
     ### FUNCTION:
     # Parameters:
     n  = n+1
@@ -29,7 +28,6 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
     OptionValue = (S*u^(0:n)*d^(n:0))
     offset = 1
     Tree = OptionValue = (abs(OptionValue)+OptionValue)/2
-
     {for (j in (n-1):0) {
       Tree <-c(Tree, rep(0, times=n-j))
       for (i in 0:j) {
@@ -38,11 +36,9 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
               (p*OptionValue[i+1+offset] +
                  (1-p)*OptionValue[i+offset]) * Df )
         Tree = c(Tree, OptionValue[i+offset]) } } }
-
     Tree = matrix(rev(Tree), byrow = FALSE, ncol = n+1)
     colnames(Tree) <- paste(0:n, sep = "")
     rownames(Tree) <- paste( 0:n, sep = "")
-
     # Binomial Lattice for State Variable:
     dx = -0.025
     dy = 0.4
@@ -66,7 +62,6 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
       x = rep(c(i+1,i), times = 2*i)[1:length(y)]
       lines(x, y, col = 2)
     }
-
     # Cashflow Matrix
     Cashflow=matrix(0, nrow = n+1, ncol = n+1)
     for(i in n:1){
@@ -82,11 +77,9 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
         }
       }
     }
-
     colnames(Cashflow) <- paste(0:n, sep = "")
     rownames(Cashflow) <- paste( 0:n, sep = "")
     Cashflow = round(Cashflow, digits = 2)
-
     # DecisionTree Matrix
     DecisionTree = as.data.frame(Cashflow)
     for(i in 1:n){
@@ -98,7 +91,6 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
         }
       }
     }
-
     # Triangle(DecisionTree)
     for(i in 0:n+1){
       for(j in 0:n+1){
@@ -108,10 +100,8 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
         }
       }
     }
-
     colnames(DecisionTree) <- paste("Year ", 0:n, sep = "")
     rownames(DecisionTree) <- paste( 0:n, sep = "")
-
     Sample_path = rbinom(n,1,0.5)
     Decision = c()
     Decision_Path = vector()
@@ -138,7 +128,6 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
     print(Cashflow)
     print("Decision Tree:")
     print(DecisionTree[0:n,0:n])}
-
 #' @title Binomial Real Options Pricing with Monte Carlo Simulation
 #' @description This R function will provide real option prices and the probability of investment within the investment horizon using binomial lattice and monte carlo simulations.
 #' @param S State Variable
@@ -152,21 +141,9 @@ BinomialTree <- function(S, I, Time, r, sigma, n)
 #' @examples BinomialTree_MC(S=70, I=100, Time=1, r=.05, sigma=0.3, n=10, MC_loops = 1000)
 #' @export
 BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
-  {   # A function implemented by Diethelm Wuertz
-
-  # Description:
-  #   Calculates option prices from the Cox-Ross-Rubinstein
-  #   Binomial tree model.
-
-  # Note:
-  #   The model described here is a version of the CRR Binomial
-  #   Tree model.
-
+  {
   # Example:
   # BinomialTree_MC(S=70, I=100, Time=1, r=.05, sigma=0.3, n=10, MC_loops = 1000)
-
-  # FUNCTION:
-
   # Parameters:
   n  = n+1
   dt = Time / (n-1)
@@ -179,7 +156,6 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
   OptionValue = (S*u^(0:n)*d^(n:0))
   offset = 1
   Tree = OptionValue = (abs(OptionValue)+OptionValue)/2
-
   {for (j in (n-1):0) {
     Tree <-c(Tree, rep(0, times=n-j))
     for (i in 0:j) {
@@ -188,12 +164,10 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
             (p*OptionValue[i+1+offset] +
                (1-p)*OptionValue[i+offset]) * Df )
       Tree = c(Tree, OptionValue[i+offset]) } } }
-
   Tree = matrix(rev(Tree), byrow = FALSE, ncol = n+1)
   colnames(Tree) <- paste(0:n, sep = "")
   rownames(Tree) <- paste( 0:n, sep = "")
   # Tree Output:
-
   # Binomial Lattice for State Variable:
   dx = -0.025
   dy = 0.4
@@ -217,7 +191,6 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
     x = rep(c(i+1,i), times = 2*i)[1:length(y)]
     lines(x, y, col = 2)
   }
-
   #Cashflow Matrix
   Cashflow=matrix(0, nrow = n+1, ncol = n+1)
   for(i in n:1){
@@ -233,8 +206,6 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
       }
     }
   }
-
-
   colnames(Cashflow) <- paste(0:n, sep = "")
   rownames(Cashflow) <- paste( 0:n, sep = "")
   Cashflow = round(Cashflow, digits = 2)
@@ -258,10 +229,8 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
       }
     }
   }
-
   colnames(DecisionTree) <- paste("Year ", 0:n, sep = "")
   rownames(DecisionTree) <- paste( 0:n, sep = "")
-
   Decision_Mat= matrix(0, 1, n-1)
   for (i in 0:MC_loops){
     Sample_path = rbinom(n-1,1,0.5)
@@ -277,7 +246,6 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
         x_position = x_position+1
         y_position = y_position+1
         Decision_Path = append(Decision_Path,DecisionTree[x_position,y_position])}
-
     }
     Decision_Mat = rbind.data.frame(Decision_Path,Decision_Mat)
   }
@@ -285,11 +253,9 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
   Decision_Mat$Never <- apply(Decision_Mat, 1, function(x) length(which(x=="Wait")))
   #Counting loops with no Investment
   Full_Wait <- count(Decision_Mat$Never==(n-1))
-
   Investment_Probability_Table <- ldply(Decision_Mat, function(c) sum(c=="Invest"))
   Investment_Probability_Table["V1"] = Investment_Probability_Table["V1"]/(MC_loops)
   Investment_Probability_Table[n,"V1"] = Full_Wait[2,2]/(MC_loops)
-
   #Plotting Investment Probabilities
   X_axis = paste("Year ", 1:n, sep = "")
   X_axis[n] = "Never"
@@ -299,8 +265,6 @@ BinomialTree_MC <- function(S, I, Time, r, sigma, n, MC_loops)
     ggplot(Investment_Probability_Table, aes(x=as.array(X_axis), y=V1)) +
     geom_bar(stat="identity", colour="Navy") + xlab("Year") + ylab("Probability of Investment") +
     geom_text(aes(label=V1), position=position_dodge(width=0.9), vjust=-0.25)
-
-
   ######Function Outputs######
   #Printing Model Parameters
   param = c(S,Time,1+r,sigma,n-1,u,1/u,p,1-p)
