52e2036953a52481a80ab066bb7c0b7238d74f67
Bahram Abediniangrabi <abedini.bahram@gmail.com>
2021-06-11 12:38:51 -0500

    Update

--- R/ConstructionAnalytics.R
+++ R/ConstructionAnalytics.R
@@ -1,4 +1,4 @@
-#' @title Probabilistic Life Cycle Cost Analysis
+#' @title Probabilistic Life Cycle Cost Analysis Based On Net Present Value
 #' @description This R function will provide probabilistic LCCA for a projects with several uncertain initail cost components.
 #' @param comp1 A vector for the first initial cost component  #Example for comp1 = rtriangle(n_loop, a =10, b = 200, c = 30)
 #' @param comp2 A vector for the second initial cost component #Example for comp2 = rnorm(n_loop, mean=10, sd=3)
@@ -17,10 +17,11 @@ LCCA_MC=function(comp1 = NA, comp2 = NA,comp3 = NA, comp4 = NA, comp5 = NA, recu
   ####Example
   ##n_loop=10000
   ##comp1 = rtriangle(n_loop, a =10, b = 200, c = 180)
-  ##comp2 = rnorm(n_loop, mean=10, sd=10)
+  ##comp2 = rep(1000, length.out=n_loop)
   ##comp3 = rlnorm(n_loop, meanlog = 10, sdlog = 1)
   ##comp4 = rnorm(n_loop, mean=100, sd=20)
   ##comp5 = runif(n_loop, min = 100, max = 300)
+  ##comp6 = rweibull(n_loop, shape=7.5, scale=1)
   #LCCA_MC(comp1 = comp1, comp2 = comp2, comp3 = comp3, comp4 = comp4, comp5 = comp5,  r=0.05, n_loop=n_loop, n=50, recurring_cost = 100)
 
   #NULL
@@ -46,14 +47,60 @@ LCCA_MC=function(comp1 = NA, comp2 = NA,comp3 = NA, comp4 = NA, comp5 = NA, recu
   hist(npv_total_cost, col = "Gray",xlab="Net Present Values", main="Histogram of Net Present Values")
   plot(density(npv_total_cost),main="Kernel Density of Net Present Values",col="blue")
   plot(ecdf(npv_total_cost),main="Cumulative Density Function of Net Present Values",xlab="Net Present Values",col="blue")
+  print(npv_total_cost)
+}
 
-  #print(euv_total_cost)
+#' @title Probabilistic Life Cycle Cost Analysis Based On Equivalent Uniform Annual Cost
+#' @description This R function will provide probabilistic LCCA based on EUAC for a projects with several uncertain initail cost components.
+#' @param comp1 A vector for the first initial cost component  #Example for comp1 = rtriangle(n_loop, a =10, b = 200, c = 30)
+#' @param comp2 A vector for the second initial cost component #Example for comp2 = rep(100, length.out=n_loop)
+#' @param comp3 A vector for the third initial cost component  #Example for comp3 = rlnorm(n_loop, meanlog = 10, sdlog = 1)
+#' @param comp4 A vector for the forth initial cost component  #Example for comp4 = runif(n_loop, min = 0, max = 25)
+#' @param comp5 A vector for the fifth initial cost component  #Example for comp5 = runif(n_loop, min = 0, max = 25)
+#' @param recurring_cost The annual reccuring cost of the project
+#' @param r Rate of Return
+#' @param n Investment Horizon (yearly)
+#' @param n_loop Number of Monte Carlo Iterations
+#' @return Returns the histogram plot of all recurring cost components (inputs) and histogram, kernel density, and cumulative density function plots of net present value and equivalent uniform value (outputs) for the project.
+#' @examples LCCA_MC(comp1 = rnorm(10, mean=3, sd=1), r =0.05, recurring_cost = 10,  n=10, n_loop = 10)
+#' @export
+EUAC_MC=function(comp1 = NA, comp2 = NA,comp3 = NA, comp4 = NA, comp5 = NA, recurring_cost = NA, r, n, n_loop){
+  all=list(comp1,comp2,comp3,comp4,comp5,recurring_cost,r,n_loop, n)
+  ####Example
+  ##n_loop=10000
+  ##comp1 = rtriangle(n_loop, a =10, b = 200, c = 180)
+  ##comp2 = rep(100, length.out=n_loop)
+  ##comp3 = rlnorm(n_loop, meanlog = 10, sdlog = 1)
+  ##comp4 = rnorm(n_loop, mean=100, sd=20)
+  ##comp5 = runif(n_loop, min = 100, max = 300)
+  ##comp6 = rweibull(n_loop, shape=7.5, scale=1)
+  #LCCA_MC(comp1 = comp1, comp2 = comp2, comp3 = comp3, comp4 = comp4, comp5 = comp5,  r=0.05, n_loop=n_loop, n=50, recurring_cost = 100)
+
+  #NULL
+  if(any(lapply(all,is.null)==T)) stop("Cannot input any variables as NULL.")
+  if (missing(comp1)){comp1 = rep(0, length.out=n_loop)} else {comp1 = comp1}
+  if (missing(comp2)){comp2 = rep(0, length.out=n_loop)} else {comp2 = comp2}
+  if (missing(comp3)){comp3 = rep(0, length.out=n_loop)} else {comp3 = comp3}
+  if (missing(comp4)){comp4 = rep(0, length.out=n_loop)} else {comp4 = comp4}
+  if (missing(comp5)){comp5 = rep(0, length.out=n_loop)} else {comp5 = comp5}
+  if (missing(recurring_cost)){recurring_cost_list = rep(0, length.out=n)} else {
+    recurring_cost_list = rep(recurring_cost*(((1+r)^n)-1)/(r*(1+r)^n), length.out = n)}
+
+  npv_total_cost = comp1 + comp2 + comp3 + comp4 + comp5 + recurring_cost_list
+  euv_total_cost = npv_total_cost*(r*(1+r)^n)/(((1+r)^n)-1)
+
+  #print(npv_recurring_cost)
+  hist(comp1, col = "Gray",xlab="Comp1", main="Histogram of Comp1")
+  hist(comp2, col = "Gray",xlab="Comp1", main="Histogram of Comp2")
+  hist(comp3, col = "Gray",xlab="Comp1", main="Histogram of Comp3")
+  hist(comp4, col = "Gray",xlab="Comp1", main="Histogram of Comp4")
+  hist(comp5, col = "Gray",xlab="Comp1", main="Histogram of Comp5")
+
   hist(euv_total_cost, col = "Gray",xlab="Equivalent Uniform Values", main="Histogram of Equivalent Uniform Values")
   plot(density(euv_total_cost),main="Kernel Density of Equivalent Uniform Values",col="blue")
   plot(ecdf(euv_total_cost),main="Cumulative Density Function of Equivalent Uniform Values",xlab="Equivalent Uniform Values",col="blue")
-  print(npv_total_cost)
   print(euv_total_cost)
-  }
+}
 
 #' @title Real Options Pricing using Binomial Lattice
 #' @description This R function will provide real option prices for underlying assets (Infrastructure Projects, Buildings, etc) using binomial lattice.
